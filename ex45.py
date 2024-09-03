bf912130cfae6c4b305ca208b2ce7cd03d0a8ff3
bellxtzz <koklir669@gmail.com>
2024-09-03 20:58:22 -0300

    arrumei esses ex

--- ex45.py
+++ ex45.py
@@ -3,4 +3,4 @@ print ("\33[1;35;40mNome na vertical.\33[m")
 nome = input("\n\33[1;37;40mInforme um nome: \33[m")
 
 for letra in nome:
-    print ("\n\33[1;34;40m",letra,"\33[m")
+    print ("\n\33[1;35;40m",letra,"\33[m")
---
+++ ex46.py
@@ -0,0 +1,11 @@
+for():
+c = ()
+n = 0
+clear = ()
+print ("Informe o número que você deseja saber a tabuada: ")
+if (n > 10):
+     print ("Este valor é inválido, escolha outro número.\n")
+enquanto (n > 10) 
+enquanto (c <= 10) 
+print (n, "\t", "x", "\t",c, "\t", "=", "\t", n*c, "\n")
+c = c + 1
